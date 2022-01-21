27e9ca794115b0dc31ad1e6a2d971e0e5d90dec8
kenmjzuta2017 <86814215+kenmjzuta2017@users.noreply.github.com>
2022-01-21 14:29:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
-    introTitle: 'Duyên cute à!',
+    introTitle: 'Nhi cute ưiiii, Chú Sơn đâyyyy :))!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Duyên cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Lộc à :3',
+    Mà sao Nhi không nhầm đường
+    Lạc vào tym chú thế này ^^!
+    Nào có đi vào thì nói anh open the door cho nha :333!',
+    btnIntro: 'haha giởn đó lo học đi nha zui zẻ hông có quạo à',
+    title: 'Không phải Hải Phòng cho nên phải lòng vòng :333',
+    desc: 'Thứ four đi xem phim nha bé',
+    btnYes: 'chắc chắn là đồng ý rồi :))333',
+    btnNo: 'Không đi thì cũng phải đi ;))',
     question:'Trên thế giới hơn 7 tỉ người mà sao Duyên lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
